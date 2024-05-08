e6d87e334b7f292baaa06a31adbb78c577013097
jacksonhuish88 <jacksonhuish88@gmail.com>
2024-05-08 06:37:43 -0600

    iframe?

--- src/pages/components/Contact-Us.js
+++ src/pages/components/Contact-Us.js
@@ -15,7 +15,7 @@ function ContactUs() {
         </div>
         <div className="col-lg-6 col-md-8 col-sm-10 col-xs-12 p-3">
           <iframe 
-            src="https://docs.google.com/forms/d/e/1FAIpQLSfbuASJwta9RLYGE5ybVgpGi6x01ll6JHlZOoKUmln8iEc-_Q/viewform?embedded=true" 
+            src="//docs.google.com/forms/d/e/1FAIpQLSfbuASJwta9RLYGE5ybVgpGi6x01ll6JHlZOoKUmln8iEc-_Q/viewform?embedded=true" 
             style={{ width: '100%', height: '450px', border: '0' }}
             title="Fore Seasons Contact">Loading…</iframe>
         </div>
