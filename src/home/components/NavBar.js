512dad5413d0324109cf3a1ada8a5f09cda62cce
jacksonhuish88 <jacksonhuish88@gmail.com>
2024-05-08 04:07:18 -0600

    instagram fix

--- src/home/components/NavBar.js
+++ src/home/components/NavBar.js
@@ -25,7 +25,7 @@ function Navigation() {
             <Nav.Link href="#materials">Materials</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#contact">Contact</Nav.Link>
-            <Nav.Link href="https://www.instagram.com/yourcompany" aria-label="Instagram">
+            <Nav.Link href="https://www.instagram.com/fore.seasons.golf/" aria-label="Instagram">
               <FontAwesomeIcon icon={faInstagram} size="lg" />
             </Nav.Link>
           </Nav>
